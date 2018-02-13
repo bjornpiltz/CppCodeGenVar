97a3c1c115067789a24dccc271333ba96dd64f01
Björn Piltz <bjornpiltz@gmail.com>
2018-02-13 18:32:18 +0100

    Added example eigen_inverse.

--- include/codegenvar/SymbolWithEigenBindings.h
+++ include/codegenvar/SymbolWithEigenBindings.h
@@ -2,31 +2,6 @@
 #include "Symbol.h"
 #include <Eigen/Core>
 
-namespace codegenvar
-{
-    typedef Eigen::Matrix<Symbol, -1, 1> Vec;
-    typedef Eigen::Matrix<Symbol, -1, -1> Mat;
-    typedef Eigen::Matrix<Symbol, 2, 2> Mat2;
-    typedef Eigen::Matrix<Symbol, 2, 3> Mat23;
-    typedef Eigen::Matrix<Symbol, 3, 3> Mat3;
-    typedef Eigen::Matrix<Symbol, 2, 1> Vec2;
-    typedef Eigen::Matrix<Symbol, 3, 1> Vec3;
-    
-
-    /* For example:
-     * Vec2 x = namedVector<2>("x) 
-     * creates a vector with symbols named "x_1" and "x_2".
-     */
-    template <int n = -1>
-    Eigen::Matrix<Symbol, n, 1> namedVector(std::string name, int size = -1)
-    {
-        Eigen::Matrix<Symbol, n, 1> result(n == -1 ? size : n);
-        for (int i= 0; i<n; i++)
-            result(i) = Symbol(name + "_" + std::to_string(i));
-        return result;
-    }
-}
-
 // The following lets us put Symbols in Eigen matrices:
 namespace Eigen
 {
@@ -77,3 +52,46 @@ struct ScalarBinaryOpTraits<double, codegenvar::Symbol, BinaryOp>
 };
 
 }  // namespace Eigen
+
+namespace codegenvar
+{
+    typedef Eigen::Matrix<Symbol, -1, 1> Vec;
+    typedef Eigen::Matrix<Symbol, -1, -1> Mat;
+    typedef Eigen::Matrix<Symbol, 2, 2> Mat2;
+    typedef Eigen::Matrix<Symbol, 2, 3> Mat23;
+    typedef Eigen::Matrix<Symbol, 3, 3> Mat3;
+    typedef Eigen::Matrix<Symbol, 2, 1> Vec2;
+    typedef Eigen::Matrix<Symbol, 3, 1> Vec3;
+    
+
+    /* For example:
+     * Vec2 x = namedVector<2>("x) 
+     * creates a vector with symbols named "x_1" and "x_2".
+     */
+    template <int n = -1>
+    Eigen::Matrix<Symbol, n, 1> namedVector(std::string name, int size = -1)
+    {
+        Eigen::Matrix<Symbol, n, 1> result(n == -1 ? size : n);
+        for (int i= 0; i<n; i++)
+            result(i) = Symbol(name + "_" + std::to_string(i+1));
+        return result;
+    }
+    Mat namedMatrix(std::string name, int rows, int cols)
+    {
+        Mat result(rows, cols);
+        for (int r = 0; r<rows; r++)
+            for (int c = 0; c<cols; c++)
+                result(r, c) = Symbol(name + "_" + std::to_string(r+1) + std::to_string(c+1));
+        return result;
+    }
+    template<typename Derived>
+    void operator |=(Eigen::DenseBase<Derived> & dst, const Eigen::DenseBase<Derived> & src)
+    {
+        if (dst.rows() != src.rows() || dst.cols() != src.cols())
+            dst.resize(src.rows(), src.cols());
+    
+        for (int r = 0; r<src.rows(); r++)
+            for (int c = 0; c<src.cols(); c++)
+                dst(r, c) |= src(r, c);
+    }
+}
