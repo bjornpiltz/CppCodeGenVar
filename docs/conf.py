fcbff6746baaf6e0a01a21078a79d5698f1a142f
bjornpiltz <bjornpiltz@gmail.com>
2018-02-28 12:59:50 +0100

    Fixed warning about static path missing.

--- docs/conf.py
+++ docs/conf.py
@@ -45,7 +45,7 @@ extensions = ['sphinx.ext.doctest',
     'sphinx.ext.ifconfig',  
     'sphinx_tabs.tabs',
     'sphinx.ext.githubpages',
-	'breathe']
+    'breathe']
 
 # Breathe configurations
 breathe_projects = {"cppcodegenvar": "doxyout/xml"}
@@ -114,7 +114,7 @@ html_theme = 'sphinx_rtd_theme'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = []
 import sphinx_rtd_theme
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
